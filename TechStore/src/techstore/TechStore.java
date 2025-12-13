8704035a5b17ae3df919909c8a87fe90929392a9
sudetaskan <sude_taskann@outlook.com>
2025-12-13 23:39:58 +0300

    bağlantı cevabı

--- TechStore/src/techstore/TechStore.java
+++ TechStore/src/techstore/TechStore.java
@@ -15,6 +15,7 @@ public class TechStore {
      */
     public static void main(String[] args) {
         // Sude bu yorumu görüyorsan bağlantı başarılıdır
+        // mesajını aldım Beyza 
     }
     
 }
