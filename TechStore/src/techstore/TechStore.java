ac3542af67637e8d0543404c4c59e5dab378e22e
Beyza Yıldırım <beyza.yildirim9@icloud.com>
2025-12-13 23:26:15 +0300

    Bağlantı testi

--- TechStore/src/techstore/TechStore.java
+++ TechStore/src/techstore/TechStore.java
@@ -14,7 +14,7 @@ public class TechStore {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Sude bu yorumu görüyorsan bağlantı başarılıdır
     }
     
 }
