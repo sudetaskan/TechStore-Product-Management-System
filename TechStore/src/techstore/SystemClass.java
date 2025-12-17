b54d46b7c84b2b820cd6d45937cb96d4fdd7397b
Beyza Yıldırım <beyza.yildirim9@icloud.com>
2025-12-17 16:35:48 +0300

    Frame'ler eklendi.

---
+++ TechStore/src/techstore/SystemClass.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package techstore;
+
+import java.util.ArrayList;
+import java.util.List;
+import techstore.models.ElectronicDevice;
+import techstore.models.Laptop;
+
+
+/**
+ *
+ * @author SUDE TAŞKAN
+ */
+public class SystemClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static int stockAmount_L=4,stockAmount_T=4,stockAmount_S=4;
+    
+    public static int getStockAmount_L() {
+        return stockAmount_L;
+    }
+
+    public static int getStockAmount_T() {
+        return stockAmount_T;
+    }
+
+    public static int getStockAmount_S() {
+        return stockAmount_S;
+    }
+    public static void displayDevice( ){
+        
+    }
+    public static void addDeviceLaptop(List<? extends ElectronicDevice> device){
+        
+    }
+    public static void deleteDevice(String serialNum){
+        
+    }
+    public static void searchDevice(String serialNum){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+    public static void addDeviceForManager(String serialNum){
+        
+    }
+}
